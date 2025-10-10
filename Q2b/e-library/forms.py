97850bae64a72632dc1f6672166c2ba7e37b55ba
joshua <joshuaebenezar14@gmail.com>
2025-10-10 15:32:49 +0800

    added author and book sub forms in forms.py

--- Q2b/e-library/forms.py
+++ Q2b/e-library/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, Length, EqualTo
+from wtforms import StringField, PasswordField, SubmitField, SelectMultipleField, SelectField, TextAreaField, IntegerField, FieldList, FormField, BooleanField
+from wtforms.validators import DataRequired, Email, Length, EqualTo, URL
 
 class RegisterForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
@@ -11,4 +11,21 @@ class RegisterForm(FlaskForm):
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class AuthorForm(FlaskForm):
+    name = StringField('Author Name')
+    is_illustrator = BooleanField('Illustrator')
+
+class AddBookForm(FlaskForm):
+    genres = SelectMultipleField('Choose multiple Genres', coerce=str, validators=[DataRequired()])
+    title = StringField('Title', validators=[DataRequired()])
+    category = SelectField('Choose a category', choices=[('Children', 'Children'), ('Teens', 'Teens'), ('Adult', 'Adult')], validators=[DataRequired()])
+    url = StringField('URL for Cover', validators=[DataRequired(), URL()])
+    description = TextAreaField('Description', validators=[DataRequired()])
+
+    authors = FieldList(FormField(AuthorForm), min_entries=5, max_entries=5)
+    
+    pages = IntegerField('Number of pages', validators=[DataRequired()])
+    copies = IntegerField('Number of copies', validators=[DataRequired()])
     submit = SubmitField('Submit')
